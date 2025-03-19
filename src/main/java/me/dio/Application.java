da18f6e4d7f36d8d7bbc4641edcd7ebe37c49a6e
Gabriel Herculano <100312430+GabrielVictor2022@users.noreply.github.com>
2025-03-19 18:48:30 -0300

    Decola Tech 2025

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,13 @@
+package me.dio.decola_tech_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechSpringApplication.class, args);
+	}
+
+}
